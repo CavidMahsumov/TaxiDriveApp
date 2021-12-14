d1406ac6cb10951dacb4f81371b78773d3147384
Cavid <mehsumovcavid@gmail.com>
2021-12-14 17:49:08 +0400

    Add project files.

---
+++ TaxiDriveApp/Models/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiDriveApp.Models
+{
+    public class Helper
+    {
+        static public MainWindow mainWindow { get; set; }
+        public Helper()
+        {
+            mainWindow = new MainWindow();
+        }
+    }
+}
---
+++ TaxiDriveApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using TaxiDriveApp.Commands;
+
+namespace TaxiDriveApp.ViewModel
+{
+   public class MainWindowViewModel:BaseViewModel
+    {
+        public RelayCommand MouseDown { get; set; }
+        public RelayCommand MouseDownForReset { get; set; }
+        public RelayCommand MouseDownForSend { get; set; }
+
+        public MainWindowViewModel(MainWindow mainWindow)
+        {
+            MouseDown = new RelayCommand((sender) =>
+            {
+
+                Storyboard beginStoryboard = new Storyboard();
+                beginStoryboard = mainWindow.FindResource("Storyboard1") as Storyboard;
+                beginStoryboard.Begin();
+
+            });
+            MouseDownForReset = new RelayCommand((sender) => {
+
+                Storyboard beginStoryboard = new Storyboard();
+                beginStoryboard = mainWindow.FindResource("ResetStoryBoard") as Storyboard;
+                beginStoryboard.Begin();
+
+
+            });
+            MouseDownForSend = new RelayCommand((sender) =>
+            {
+                //mainWindow.Login.Visibility = Visibility.Visible;
+                
+
+
+            });
+        }
+    }
+}
