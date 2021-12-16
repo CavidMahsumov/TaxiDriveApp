0e85c2275f21ec8435ba42f523814610fd5f2f49
Mahs_kz07@itstep.edu.az <Mahs_kz07@itstep.edu.az>
2021-12-16 16:54:29 +0400

    LoginComp

---
+++ TaxiDriveApp/ViewModel/LoginUserControlViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using TaxiDriveApp.Commands;
+using TaxiDriveApp.View;
+
+namespace TaxiDriveApp.ViewModel
+{
+    public class LoginUserControlViewModel : BaseViewModel
+    {
+        public RelayCommand MouseDownPulseButton { get; set; }
+        public RelayCommand MouseDownResetButton { get; set; }
+        public RelayCommand MouseDownSendButton { get; set; }
+        public RelayCommand MouseDownForRegisterNextButton { get; set; }
+        public LoginUserControlViewModel(LoginUserCOntrol loginUserControl)
+        {
+          
+           
+            MouseDownPulseButton = new RelayCommand((sender) =>
+            {
+
+                Storyboard beginStoryboard = new Storyboard();
+                beginStoryboard = loginUserControl.FindResource("Storyboard1") as Storyboard;
+                beginStoryboard.Begin();
+
+            });
+            MouseDownResetButton = new RelayCommand((sender) =>
+            {
+
+                Storyboard beginStoryboard = new Storyboard();
+                beginStoryboard = loginUserControl.FindResource("ResetStoryBoard") as Storyboard;
+                beginStoryboard.Begin();
+
+            });
+            MouseDownSendButton = new RelayCommand((sender) =>
+            {
+                for (int i = 0; i < loginUserControl.ResetPassword.Children.Count; i++)
+                {
+                    loginUserControl.ResetPassword.Children.RemoveAt(i);
+                }
+
+                for (int i = 0; i < loginUserControl.Login.Children.Count; i++)
+                {
+                    loginUserControl.Login.Children.RemoveAt(i);
+                }
+                for (int i = 0; i < loginUserControl.Register.Children.Count; i++)
+                {
+                    loginUserControl.Register.Children.RemoveAt(i);
+                }
+               
+                LoginUserCOntrol loginUserCOntrol = new LoginUserCOntrol();
+                loginUserControl.MainGrid.Children.Add(loginUserCOntrol);
+
+            });
+            MouseDownForRegisterNextButton = new RelayCommand((sender) =>
+            {
+                for (int i = 0; i < loginUserControl.ResetPassword.Children.Count; i++)
+                {
+                    loginUserControl.ResetPassword.Children.RemoveAt(i);
+                }
+
+                for (int i = 0; i < loginUserControl.Login.Children.Count; i++)
+                {
+                    loginUserControl.Login.Children.RemoveAt(i);
+                }
+                for (int i = 0; i < loginUserControl.Register.Children.Count; i++)
+                {
+                    loginUserControl.Register.Children.RemoveAt(i);
+                }
+              
+                LoginUserCOntrol loginUserCOntrol = new LoginUserCOntrol();
+                loginUserControl.MainGrid.Children.Add(loginUserCOntrol);
+                
+
+            });
+        }
+    }
+}
--- TaxiDriveApp/ViewModel/MainWindowViewModel.cs
+++ TaxiDriveApp/ViewModel/MainWindowViewModel.cs
@@ -15,6 +15,7 @@ namespace TaxiDriveApp.ViewModel
         public RelayCommand MouseDown { get; set; }
         public RelayCommand MouseDownForReset { get; set; }
         public RelayCommand MouseDownForSend { get; set; }
+        public RelayCommand MouseDownForRegisterNextButton { get; set; }
 
         public MainWindowViewModel(MainWindow mainWindow)
         {
@@ -53,6 +54,27 @@ namespace TaxiDriveApp.ViewModel
                 }
                 LoginUserCOntrol loginUserCOntrol = new LoginUserCOntrol();
                 mainWindow.MainGrid.Children.Add(loginUserCOntrol);
+            });
+            MouseDownForRegisterNextButton = new RelayCommand((sender) => {
+
+
+                for (int i = 0; i < mainWindow.ResetPassword.Children.Count; i++)
+                {
+                    mainWindow.ResetPassword.Children.RemoveAt(i);
+                }
+
+                for (int i = 0; i < mainWindow.Login.Children.Count; i++)
+                {
+                    mainWindow.Login.Children.RemoveAt(i);
+                }
+                for (int i = 0; i < mainWindow.Register.Children.Count; i++)
+                {
+                    mainWindow.Register.Children.RemoveAt(i);
+                }
+                LoginUserCOntrol loginUserCOntrol = new LoginUserCOntrol();
+                mainWindow.MainGrid.Children.Add(loginUserCOntrol);
+
+
             });
         }
     }
